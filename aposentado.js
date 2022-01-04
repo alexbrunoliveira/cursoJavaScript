b1269d07041ff13a8e4252f8aa568b99f7723008
alexbrunoliveira <alexbrunoliveira@gmail.com>
2022-01-04 14:45:55 -0300

    first commit

---
+++ aposentado.js
@@ -0,0 +1,7 @@
+var idade = 66
+
+if(idade > 65 ) {
+    console.log( " Você já pode se aposentar ")
+} else { 
+    console.log ( "Você ainda não pode se aposentar ")
+}
---
+++ parOuImpar.js
@@ -0,0 +1,8 @@
+var numeroDaSorte = 31
+
+
+if ( numeroDaSorte % 2 === 0) {
+    console.log (" O número é par")
+} else {
+    console.log ("O número é ímpar ")
+}
